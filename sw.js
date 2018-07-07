d899ba22203106a3af460905f167d1f8f0f3d44e
tianmaxk <tianmaxk@126.com>
2018-07-07 23:32:55 +0800

    改造sw.js

--- sw.js
+++ sw.js
@@ -5,20 +5,18 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(cacheName)
     .then(cache => cache.addAll([
-      './js/main.js',
-      './js/article.js',
-      './images/newspaper.svg',
-      './css/site.css',
-      './data/latest.json',
-      './data/data-1.json',
-      './article.html',
-      './index.html'
+      //'./js/main.js'
     ]))
   );
 });
 
 // Cache any new resources as they are fetched
 self.addEventListener('fetch', function(event) {
+  var requrl = event.request?event.request.url:'';
+  //不处理POST或者跨域请求或者http请求，也不处理非dist下的文件请求
+  if (requrl && (event.request.method !== 'GET' || !requrl.startsWith(origin) || requrl.indexOf('/sw.js')==-1)){
+  	return;
+  }
   event.respondWith(
     caches.match(event.request, { ignoreSearch: true })
     .then(function(response) {
