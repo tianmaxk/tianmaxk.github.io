41460f51bc8595f940888f0f7de033511324fa36
taomin <taomin@whptp.com>
2018-07-10 17:13:58 +0800

    流功能，边下载边渲染

---
+++ streamrender/sw.js
@@ -0,0 +1,60 @@
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  const requestURL = new URL(event.request.url);
+
+  if (requestURL.origin != location.origin) return;
+
+  if (requestURL.pathname.endsWith('index.html')) {
+    event.respondWith(htmlStream());
+  }
+});
+
+function htmlStream() {
+	return fetch('./error.html').then(response => {
+	    // response.body is a readable stream.
+	    // Calling getReader() gives us exclusive access to
+	    // the stream's content
+	    var reader = response.body.getReader();
+	    var bytesReceived = 0;
+
+	    const stream = new ReadableStream({
+		  start: controller => {
+			// read() returns a promise that resolves
+		    // when a value has been received
+		    reader.read().then(function processResult(result) {
+		        // Result objects contain two properties:
+		        // done  - true if the stream has already given
+		        //         you all its data.
+		        // value - some data. Always undefined when
+		        //         done is true.
+		        if (result.done) {
+		           console.log("Fetch complete");
+		           controller.close();
+		           return;
+		        }
+	
+		        // result.value for fetch streams is a Uint8Array
+		        bytesReceived += result.value.length;
+		        console.log('Received', bytesReceived, 'bytes of data so far');
+		        controller.enqueue(result.value);
+
+		        // Read some more, and call this function again
+		        return reader.read().then(processResult);
+		    });
+		  }
+		});
+
+		return new Response(stream, {
+		  headers: {
+		    'Content-Type': 'text/html'
+		  }
+		});
+	});
+}
